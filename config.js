82b2dc222afc175562f7116513342d9266bc6d2d
jfebreg <jfebreg@gmail.com>
2026-07-05 13:28:02 -0400

    Conectar frontend con backend Render

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.REPORTE_DESVIOS_CONFIG = {
-  apiBaseUrl: "",
-  apiToken: ""
+  apiBaseUrl: "https://reporte-desvios-api.onrender.com",
+  apiToken: "WcCN45yJNXmerYOEFEzV0lwZpY94TGjqP3LZxNqg/tg="
 };
